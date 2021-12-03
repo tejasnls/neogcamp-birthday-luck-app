d8313596167e28c7e5f29cf5848deedbfcd34677
tejasnls <95417948+tejasnls@users.noreply.github.com>
2021-12-03 20:04:28 +0530

    styles

--- src/components/LuckCheck.jsx
+++ src/components/LuckCheck.jsx
@@ -5,47 +5,56 @@ const LuckCheck = () => {
   const [message, setmessage] = useState("");
   const [dob, setdob] = useState();
   const [number, setnumber] = useState();
+
   const changeHandler = (e) => {
-      const {name,value} = e.target;
-      if (name === 'dob') {
-        setdob(value);
-      }
-      else if (name === 'luckynum') {
-          setnumber(number);
-      }
-  }
+    const { name, value } = e.target;
+    if (name === "dob") {
+      setdob(value);
+    } else if (name === "luckynum") {
+      setnumber(value);
+    }
+  };
 
   const dateSum = (dob) => {
-    let sum=0;
-    let dobNumber = dob.replaceAll('-','');
-    [...dobNumber].forEach(n => sum+= (+n))
-    return sum; 
-  }
+    let sum = 0;
+    let dobNumber = dob.replaceAll("-", "");
+    [...dobNumber].forEach((n) => (sum += Number(n)));
+    return sum;
+  };
 
-  const calculateLuck = (dateSum,luckyNumber) => {
-    if (dateSum % luckyNumber === 0) {
-        setmessage("That's Lucky!")
+  const calculateLuck = (sum) => {
+    if (sum % number === 0) {
+      setmessage("That's Lucky!");
+    } else {
+      setmessage("Unlucky!");
     }
-    else {
-        setmessage("Unlucky!")
-    }
-  }
+  };
 
   const clickHandler = (e) => {
     e.preventDefault();
-    calculateLuck(dateSum(dob),number);
+    calculateLuck(dateSum(dob));
   };
-    return (
+  return (
     <div className="container">
-      <h2>Please enter your Birthday and Lucky Numeber</h2>
-      <div className="input">
-        <label htmlFor="dob">DOB</label>
-        <input onChange={changeHandler} type="date" name="dob" value ={dob} id="dob" />
-        <label htmlFor="luckynum">Lucky Number</label>
-        <input onChange={changeHandler} type="number" min="1" step="1" value ={number} name= "luckynum" id="luckynum" />
+      <div className="content-div">
+        <h2>Please enter your Birthday and Lucky Numeber</h2>
+        <div className="input">
+          <label htmlFor="dob">DOB</label>
+          <input onChange={changeHandler} type="date" name="dob" id="dob" />
+          <label htmlFor="luckynum">Lucky Number</label>
+          <input
+            onChange={changeHandler}
+            type="number"
+            min="1"
+            step="1"
+            name="luckynum"
+          />
+        </div>
+        <button type="submit" onClick={clickHandler}>
+          Check!
+        </button>
+        <span className="message">{message}</span>
       </div>
-      <button type="submit" onClick={clickHandler}>Check!</button>
-      <span className="message">{message}</span>
     </div>
   );
 };
