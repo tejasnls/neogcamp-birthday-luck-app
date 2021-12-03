694e0878c553b2a1c0f6ab30bab2f01f9815a204
tejasnls <95417948+tejasnls@users.noreply.github.com>
2021-12-03 19:00:52 +0530

    Initial setup with header and footer

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <React.Fragment>
+            <nav className="header" > 
+            <h1>Is your Birthday Lucky?</h1>
+            </nav>
+        </React.Fragment>
+    )
+}
+
+export default Header;
